bcb06fd9b1900776b83fe708597d96bb2b8283c3
Tayya-D <tayybahdar@gmail.com>
2025-03-12 17:11:14 +0000

    corrected policy json post terrafrorm validate + terraform fmt to tidy up code

--- infra/main.tf
+++ infra/main.tf
@@ -1,17 +1,17 @@
 resource "aws_lambda_function" "myfunc" {
-    filename                = data.archive_file.zip.output_path
-    source_code_hash        = data.archive_file.zip.output_base64sha256
-    function_name           = "myfunc"
-    role                    = aws_iam_role.iam_for_lambda.arn
-    handler                 = "func.handler"
-    runtime                 = "python3.8"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  function_name    = "myfunc"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "func.handler"
+  runtime          = "python3.8"
 
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-    name = "iam_for_lambda"
+  name = "iam_for_lambda"
 
-    assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
         "Statement": [
@@ -30,12 +30,12 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_resume_project" {
+  name        = "aws_iam_policy_for_terraform_resume_project_policy"
+  path        = "/"
+  description = "AWS IAM Policy for managing the resume project role"
 
-    name = "aws_iam_policy_for_terraform_resume_project_policy"
-    path = "/"
-    description = "AWS IAM Policy for managing the resume project role"
-    policy = jsondecode(
-        {
+  policy = <<EOF
+{
     "Version": "2012-10-17",
     "Statement": [
         {
@@ -64,30 +64,32 @@ resource "aws_iam_policy" "iam_policy_for_resume_project" {
             "Resource": "*"
         }
     ]
-    })
+}
+EOF
 }
 
+
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-    role = aws_iam_role.iam_for_lambda
-    policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
+  role       = aws_iam_role.iam_for_lambda
+  policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
 }
 
 data "archive_file" "zip" {
-    type = "zip"
-    source_dir = "${path.module}/lambda/"
-    output_path = "${path.module}/packedlambda.zip" 
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/"
+  output_path = "${path.module}/packedlambda.zip"
 }
 
 resource "aws_lambda_function_url" "url1" {
-    function_name = aws_lambda_function.myfunc.function_name
-    authorization_type = "NONE"
+  function_name      = aws_lambda_function.myfunc.function_name
+  authorization_type = "NONE"
 
-    cors {
-        allow_credentials = true
-        allow_origins = ["*"] # can set this to the website name so only the wesite can access the lambda
-        allow_methods = ["*"]
-        allow_headers = ["date", "keep-alive"]
-        expose_headers = ["keep-alive", "date"]
-        max_age = 86400
-    }
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"] # can set this to the website name so only the wesite can access the lambda
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
 }
